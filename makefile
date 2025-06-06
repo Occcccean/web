ee984fd6eeaa6c4000a61f1f785b00d984a8341b
lingshin <yokaringx@outlook.com>
2025-06-06 21:05:22 +0800

    runnable
    tomcat sucks

--- makefile
+++ makefile
@@ -1,6 +1,12 @@
+ip = 47.96.254.64
+url = 'http://${ip}/myapp/test?id=1&name=hello'
+
 run:
 	mvn package
 	scp target/web.war lingshin@gungnir.top:/tmp/webapps/lingshin/web.war
 
 clean:
 	mvn clean
+
+test:
+	@curl ${url}
--- src/main/java/com/control/test.java
+++ src/main/java/com/control/test.java
@@ -7,27 +7,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.PreparedStatement;
 
 @WebServlet("/test")
 public class test extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            String id = req.getParameter("id");
-            String name = req.getParameter("name");
-            DataSource dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/CustomerDB");
-            PreparedStatement stmt=  dataSource.getConnection().prepareStatement("INSERT INTO student_leader(account_id,name) values (?,?)");
-            stmt.setInt(1,Integer.parseInt(id,10));
-            stmt.setString(2,name);
-            int line = stmt.executeUpdate();
-            resp.getWriter().write("ok, updated "+line+" line");
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    try {
+      String id = req.getParameter("id");
+      String name = req.getParameter("name");
+      DataSource dataSource = (DataSource) new InitialContext().lookup("java:comp/env/DB");
+      PreparedStatement stmt = dataSource.getConnection()
+          .prepareStatement("INSERT INTO student_leader(account_id,name) values (?,?)");
+      stmt.setInt(1, Integer.parseInt(id, 10));
+      stmt.setString(2, name);
+      int line = stmt.executeUpdate();
+      resp.getWriter().write("ok, updated " + line + " line");
+    } catch (Exception e) {
+      resp.getWriter().write(e.getMessage());
     }
+  }
 }
