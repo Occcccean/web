fd35789f2726c8afe2d74e691cab370b86d36dd6
lingshin <yokaringx@outlook.com>
2025-06-07 22:53:33 +0800

    write a dao

--- src/main/java/com/dao/BaseDao.java
+++ src/main/java/com/dao/BaseDao.java
@@ -1,20 +1,22 @@
 package com.dao;
+
 import java.sql.*;
 import javax.sql.DataSource;
 import javax.naming.*;
 
 public class BaseDao {
-    DataSource dataSource;
-    public BaseDao() {
-        try {
-            Context context=new InitialContext();
-            dataSource=(DataSource) context.lookup("java:comp/env/DB");
-        }catch (NamingException e){
-            e.printStackTrace();
-        }
-    }
+  DataSource dataSource;
 
-    public Connection getConnection()throws Exception{
-        return dataSource.getConnection();
+  public BaseDao() {
+    try {
+      Context context = new InitialContext();
+      dataSource = (DataSource) context.lookup("java:comp/env/DB");
+    } catch (NamingException e) {
+      e.printStackTrace();
     }
+  }
+
+  public Connection getConnection() throws Exception {
+    return dataSource.getConnection();
+  }
 }
---
+++ src/main/java/com/dao/MentorDao.java
@@ -0,0 +1,89 @@
+package com.dao;
+
+import java.util.ArrayList;
+
+import com.model.Mentor;
+
+public class MentorDao extends BaseDao {
+  public boolean add(Mentor mentor) {
+    var sql = "Insert into mentor values(?,?,?)";
+    try {
+      var statement = getConnection().prepareStatement(sql);
+      statement.setString(1, mentor.getName());
+      statement.setString(2, mentor.getCollege());
+      statement.setLong(3, mentor.getAccount_id());
+      statement.executeUpdate();
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+  }
+
+  public Mentor getById(long id) {
+    var mentor = new Mentor();
+    var sql = "Select name, college, account_id from mentor where id = ?";
+    try {
+      var statement = getConnection().prepareStatement(sql);
+      statement.setLong(1, id);
+      var result = statement.executeQuery();
+      mentor.setId(id);
+      mentor.setName(result.getString("name"));
+      mentor.setCollege(result.getString("name"));
+      mentor.setAccount_id(result.getLong("account_id"));
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+    return mentor;
+  }
+
+  public boolean update(Mentor mentor) {
+    var sql = "Update mentor set name=?, college=?, account_id=? where id = ?";
+    try {
+      var statement = getConnection().prepareStatement(sql);
+      statement.setString(1, mentor.getName());
+      statement.setString(2, mentor.getCollege());
+      statement.setLong(3, mentor.getAccount_id());
+      statement.setLong(4, mentor.getId());
+      statement.executeUpdate();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    return false;
+  }
+
+  public boolean delete(long id) {
+    var sql = "Delete from mentor where id = ?";
+    try {
+      var statement = getConnection().prepareStatement(sql);
+      statement.setLong(1, id);
+      statement.executeUpdate();
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    return false;
+  }
+
+  public ArrayList<Mentor> getAll() {
+    var list = new ArrayList<Mentor>();
+    var sql = "Select * from mentor";
+    try {
+      var statement = getConnection().prepareStatement(sql);
+      var result = statement.executeQuery();
+      while (result.next()) {
+        var mentor = new Mentor();
+        mentor.setId(result.getLong("id"));
+        mentor.setName(result.getString("name"));
+        mentor.setCollege(result.getString("name"));
+        mentor.setAccount_id(result.getLong("account_id"));
+        list.add(mentor);
+      }
+    } catch (Exception e) {
+      return null;
+    }
+    return list;
+  }
+}
